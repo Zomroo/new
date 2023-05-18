50e0a7e00e16f89b90905a92a029c392c8df0354
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-05-18 18:28:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pyrogram
 from pyrogram import filters
+from pyrogram.enums import ChatMemberStatus
 
 # API ID, bot token, and API hash
 api_id = 14091414
@@ -22,14 +23,14 @@ async def ban_command(client, message):
 
     # Check if the bot is an admin
     chat_member = await client.get_chat_member(message.chat.id, "me")
-    if chat_member.status not in ("administrator", "creator"):
+    if chat_member.status not in (ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER):
         # The bot is not an admin, so send an error message
         await message.reply("I am not an admin in this chat.")
         return
 
     # Check if the user who sent the command has ban rights
     user_member = await client.get_chat_member(message.chat.id, user_id)
-    if user_member.status not in ("administrator", "creator"):
+    if user_member.status not in (ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.OWNER):
         # The user does not have ban rights, so send an error message
         await message.reply("You do not have permission to ban users.")
         return
