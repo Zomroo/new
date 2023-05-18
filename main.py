39292905fb2e70a132f8076137f91d25e2094015
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-05-18 18:08:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,27 +16,29 @@ bot = pyrogram.Client(
 
 # Define the ban command
 @bot.on_message(filters.command("ban"))
-async def ban(message):
+async def ban(client, message):
     # Get the user ID of the user who sent the command
     user_id = message.from_user.id
 
     # Check if the bot is an admin
-    if not bot.is_admin(message.chat.id):
+    chat_member = await client.get_chat_member(message.chat.id, "me")
+    if chat_member.status != "administrator":
         # The bot is not an admin, so send an error message
         await message.reply("I am not an admin in this chat.")
         return
 
     # Check if the user who sent the command has ban rights
-    if not bot.get_chat_member(message.chat.id, user_id).status in ("administrator", "creator"):
+    user_member = await client.get_chat_member(message.chat.id, user_id)
+    if user_member.status not in ("administrator", "creator"):
         # The user does not have ban rights, so send an error message
         await message.reply("You do not have permission to ban users.")
         return
 
     # Get the user ID of the user to ban
-    user_to_ban_id = message.text.split()[1]
+    user_to_ban_id = int(message.text.split()[1])
 
     # Ban the user
-    await bot.ban_chat_member(message.chat.id, user_to_ban_id)
+    await client.kick_chat_member(message.chat.id, user_to_ban_id)
 
     # Send a confirmation message
     await message.reply(f"User {user_to_ban_id} has been banned.")
