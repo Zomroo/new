04b6efa9a555bbb009bb45f078a493bc13738617
Zoro <93994060+Zomroo@users.noreply.github.com>
2023-05-18 18:29:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -61,5 +61,44 @@ async def ban_command(client, message):
     # Send a confirmation message
     await message.reply(f"User {user_id} has been banned.")
 
+# Extract the user ID and reason from the message
+async def extract_user_and_reason(message, sender_chat=False):
+    args = message.text.strip().split()
+    text = message.text
+    user = None
+    reason = None
+    if message.reply_to_message:
+        reply = message.reply_to_message
+        # if reply to a message and no reason is given
+        if not reply.from_user:
+            if (
+                reply.sender_chat
+                and reply.sender_chat != message.chat.id
+                and sender_chat
+            ):
+                id_ = reply.sender_chat.id
+            else:
+                return None, None
+        else:
+            id_ = reply.from_user.id
+
+        if len(args) < 2:
+            reason = None
+        else:
+            reason = text.split(None, 1)[1]
+        return id_, reason
+
+    # if not reply to a message and no reason is given
+    if len(args) == 2:
+        user = text.split(None, 1)[1]
+        return await extract_userid(message, user), None
+
+    # if reason is given
+    if len(args) > 2:
+        user, reason = text.split(None, 2)[1:]
+        return await extract_userid(message, user), reason
+
+    return user, reason
+
 # Start the bot
 bot.run()
